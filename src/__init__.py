78ee59a05f36847d3dae78e9b1b9fc4373193cf5
Meyjann <saragih.meyer0821@gmail.com>
2024-05-14 16:38:40 +0900

    feat: add asr functionality

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
+from .asr import recognize_speech
 from .constant import *
 from .example import call_stentts
 from .voice_change import record, exec_voice_change, play_mp3_from_url
---
+++ src/asr.py
@@ -0,0 +1,22 @@
+'''
+asr.py
+
+This file contains the functionalities to recognize speech from audio files.
+'''
+
+from reazonspeech.espnet.asr import load_model, transcribe, audio_from_path
+import os
+
+model = load_model()
+
+def recognize_speech() -> str:
+    '''
+    Recognizes speech from an audio file using the ReazonSpeech model.
+
+    Returns:
+        string
+    '''
+    audio = audio_from_path(os.path.abspath('output.wav'))
+
+    ret = transcribe(model, audio)
+    return ret
